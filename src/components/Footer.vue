946caf869048e3fdd461e5cea48d625004407fea
longlong <wyl0418@gmail.com>
2019-12-13 13:06:24 +0800

    *

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="index">
         <b-input-group prepend="请输入内容" class="mt-3">
-            <b-form-input v-model="newMsg" @keyup.enter="send"></b-form-input>
+            <b-form-input v-if="readyState" v-model="newMsg" @keyup.enter="send"></b-form-input>
+            <b-form-input v-else v-model="newMsg"></b-form-input>
             <b-input-group-append>
-                <b-button variant="outline-success" @click="send">发送</b-button>
+                <b-button v-if="readyState" variant="outline-success" @click="send">发送</b-button>
+                <b-button v-else variant="outline-danger" disabled="disabled" @click="send">重新链接中……</b-button>
             </b-input-group-append>
         </b-input-group>
     </div>
@@ -11,6 +13,7 @@
 
 <script>
     export default {
+        props: ['readyState'],
         data() {
             return {
                 newMsg: "1111"
--- src/views/Message.vue
+++ src/views/Message.vue
@@ -34,7 +34,7 @@
                         </li>
                     </ul>
                 </div>
-                <Footer v-on:sendMessage="sendMessage"></Footer>
+                <Footer :readyState="lockReconnect" v-on:sendMessage="sendMessage"></Footer>
             </div>
         </div>
     </div>
@@ -52,6 +52,12 @@
                 m: true,
                 ipadd: "127.0.0.1:8001/ws",
                 ws: null,
+                lockReconnect: false,//是否真正建立连接
+                timeout: 2 * 1000,//30秒一次心跳
+                timeoutObj: null,//心跳心跳倒计时
+                serverTimeoutObj: null,//心跳倒计时
+                timeoutnum: null,//断开 重连倒计时
+
                 uid: Date.parse(new Date()),
                 MessageList: [
                     {
@@ -67,7 +73,7 @@
         },
 
         created: function () {
-            this.webSocket();
+            this.initWebSocket();
         },
 
         components: {
@@ -77,12 +83,78 @@
         },
         methods: {
             sendMessage: function (value) {
-                this.send(value);
+                var that = this;
+                that.websocketsend(value);
                 var element = document.getElementById('chat-messages');
                 element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
             },
-            send: function (value) {
 
+            initWebSocket() {//建立连接
+                window.console.log("开始链接");
+                const ws_url = 'ws://' + this.ipadd;
+                //建立连接
+                this.ws = new WebSocket(ws_url);
+                //连接成功
+                this.ws.onopen = this.websocketonopen;
+                //连接错误
+                this.ws.onerror = this.websocketonerror;
+                //接收信息
+                this.ws.onmessage = this.websocketonmessage;
+                //连接关闭
+                this.ws.onclose = this.websocketclose;
+            },
+
+            websocketonopen() {//连接成功事件
+                window.console.log("链接成功");
+                var that = this;
+                that.lockReconnect = true;
+                //开启心跳
+                this.start();
+            },
+
+            websocketonerror() {//连接失败事件
+                //错误
+                window.console.log("WebSocket连接发生错误");
+                //错误提示
+                //重连
+                this.reconnect();
+            },
+            websocketclose(e) {//连接关闭事件
+                var that = this;
+                that.lockReconnect = false;
+                //关闭
+                window.console.log("connection closed (" + e.code + ")");
+                //重连
+                that.reconnect();
+            },
+            websocketonmessage(e) {//接收服务器推送的信息
+                //打印收到服务器的内容
+                //数据发送
+                var that = this;
+                window.console.log("message", e);
+
+                var msg = JSON.parse(e.data);
+                if (msg.cmd === "heartbeat") {
+                    window.console.log("heartbeat");
+                    return
+                }
+
+
+                this.minID++;
+                this.m = !this.m;
+
+                that.MessageList.push({
+                    id: this.minID,
+                    username: this.m ? "张三" : "李四",
+                    msg: "asdasd",
+                    me: this.m,
+                    time: "六分钟前"
+                });
+                //收到服务器信息，心跳重置
+                this.reset();
+            },
+
+            websocketsend(msg) {//向服务器发送信息
                 var joins = JSON.stringify({
                     Cmd: "send",
                     Seq: "asdfasdf",
@@ -90,46 +162,64 @@
                         uid: this.uid,
                         email: this.email,
                         username: this.username,
-                        message: value,
+                        message: msg,
                     }
                 });
-                this.ws.send(joins);
-
-                window.console.log(value)
+                if (this.lockReconnect){
+                    this.ws.send(joins);
+                }
             },
-            webSocket: function () {
-                var self = this;
-                this.ws = new WebSocket('ws://' + this.ipadd);
-                this.ws.addEventListener("open", function (e) {
-                    window.console.log("open", e);
-                });
 
-                this.ws.addEventListener("close", function (e) {
-                    self.eventClose(e);
-                });
-
-                this.ws.addEventListener("error", function (e) {
-                    window.console.log("error", e);
-                });
-
-                this.ws.addEventListener('message', function (e) {
-                    window.console.log("message", e);
-                    this.minID++;
-                    this.m = !this.m;
-
-                    self.MessageList.push({
-                        id: this.minID,
-                        username: this.m ? "张三" : "李四",
-                        msg: "asdsad",
-                        me: this.m,
-                        time: "六分钟前"
-                    });
+            heartJson() {
+                return JSON.stringify({
+                    Cmd: "heartbeat",
+                    Seq: "asdfasdf",
+                    Data: {}
                 });
             },
-            eventClose: function (e) {
-                window.console.log("close", e);
-                window.console.log('websocket 断开: ' + e.code + ' ' + e.reason + ' ' + e.wasClean)
-                this.webSocket();
+
+            reset() {//重置心跳
+                var that = this;
+                //清除时间
+                clearTimeout(that.timeoutObj);
+                clearTimeout(that.serverTimeoutObj);
+                //重启心跳
+                that.start();
+            },
+
+            reconnect() {//重新连接
+                window.console.log("重新链接");
+                var that = this;
+                if (that.lockReconnect) {
+                    return;
+                }
+
+                //没连接上会一直重连，设置延迟避免请求过多
+                // that.timeoutnum && clearTimeout(that.timeoutnum);
+
+                that.timeoutnum = setTimeout(function () {
+                    //新连接
+                    that.initWebSocket();
+                    // that.lockReconnect = false;
+                }, 5000);
+            },
+
+            start() {//开启心跳
+                var that = this;
+                that.timeoutObj && clearTimeout(that.timeoutObj);
+                that.serverTimeoutObj && clearTimeout(that.serverTimeoutObj);
+                that.timeoutObj = setTimeout(function () {
+                    window.console.log("heartCheck");
+                    //这里发送一个心跳，后端收到后，返回一个心跳消息，
+                    if (that.ws.readyState === 1) {//如果连接正常
+                        that.ws.send(that.heartJson());
+                    } else {//否则重连
+                        window.console.log("readyState 状态异常");
+                        that.reconnect();
+                    }
+
+
+                }, that.timeout)
             },
         }
     }
